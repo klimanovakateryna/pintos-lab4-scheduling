aae73ddd77df2cc0cfd863425cf76b2a634d8aec
Shiraz428 <88095108+Shiraz428@users.noreply.github.com>
2025-11-18 10:45:21 -0500

    Track thread priority changes in mlfqs2-leavebeforeq
    Added initial and final priority tracking for thread test.

--- pintos/src/tests/threads/mlfqs2-leavebeforeq.c
+++ pintos/src/tests/threads/mlfqs2-leavebeforeq.c
@@ -18,6 +18,8 @@ struct simple_thread_data
     int iterations;             /* Iterations so far. */
     struct lock *lock;          /* Lock on output. */
     //int **op;                   /* Output buffer position. */
+	int initial_priority;
+    int final_priority;
   };
 
 #define THREAD_CNT 1
@@ -33,6 +35,8 @@ void test_mlfqs2_leavebeforeq(void){
 	data.lock = &lock;
 	data.id = 1;
 	data.iterations = 0;
+	data.initial_priority = PRI_DEFAULT;
+    data.final_priority = -1;
 
 	//set main priority to minimum to let thread run
 	printf("Main at priority %d\n",PRI_MIN);
@@ -51,6 +55,22 @@ void test_mlfqs2_leavebeforeq(void){
 
 	msg("Main thread resumed");
 
+	lock_acquire(&lock);
+    if (data.final_priority != -1) {
+        msg("Thread initial priority: %d", data.initial_priority);
+        msg("Thread final priority: %d", data.final_priority);
+        
+        if (data.final_priority < data.initial_priority) {
+            msg("PASS: Thread priority was lowered after quantum expired.");
+        } else {
+            msg("FAIL: Thread priority was not lowered. Expected < %d, got %d",
+                data.initial_priority, data.final_priority);
+        }
+    } else {
+        msg("FAIL: Thread did not record final priority.");
+    }
+    lock_release(&lock);
+
 	msg("Test done. Thread should have finished before its quantum expired.");
 
 }
@@ -65,7 +85,9 @@ simple_thread_func (void *data_)
     {
       lock_acquire (data->lock);
       data->iterations++;
+	  data->initial_priority = thread_get_priority();	
       msg("Thread iteration %d complete", i + 1);
+	  data->final_priority = thread_get_priority();
       lock_release (data->lock);
       thread_yield ();
     }
